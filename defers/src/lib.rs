ac8f0f5ef65c30792da14d768f10363184c446aa
GHowe <116420022+GageHowe@users.noreply.github.com>
2026-06-01 16:34:14 -0500

    add simple implementation

---
+++ defers/src/lib.rs
@@ -0,0 +1,38 @@
+pub struct Defer<F: FnOnce()> {
+    f: Option<F>,
+}
+
+impl<F: FnOnce()> Defer<F> {
+    pub fn new(f: F) -> Self {
+        Self { f: Some(f) }
+    }
+
+    pub fn cancel(mut self) {
+        self.f = None;
+    }
+}
+
+impl<F: FnOnce()> Drop for Defer<F> {
+    fn drop(&mut self) {
+        if let Some(f) = self.f.take() {
+            f();
+        }
+    }
+}
+
+macro_rules! defer {
+    ($($body:tt)*) => {
+        let _defer = Defer::new(|| { $($body)* });
+    };
+}
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
---
+++ defers/target/debug/deps/defers-1dcf04e833994b49.d
@@ -0,0 +1,5 @@
+C:\Users\HoweGD\Documents\GitHub\defers\defers\target\debug\deps\defers-1dcf04e833994b49.d: src\lib.rs
+
+C:\Users\HoweGD\Documents\GitHub\defers\defers\target\debug\deps\libdefers-1dcf04e833994b49.rmeta: src\lib.rs
+
+src\lib.rs:
